0f01bcae67e7975b0562bc0d710659124e9f2538
sourav-dn <souravdn.dev@gmail.com>
2025-07-12 15:04:22 +0600

    add privateroute

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../hooks/useAuth";
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+
+    if (user) {
+        return children;
+    }
+
+    if (loading) {
+        return <Loading />
+    }
+
+
+    return <Navigate to='/login' state={{ form: location }} replace />
+};
+
+export default PrivateRoute;
